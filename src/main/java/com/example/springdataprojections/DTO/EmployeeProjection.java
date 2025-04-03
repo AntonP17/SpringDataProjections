85d87d2cfcf04973bfc053a8c8720868c06e584f
AntonP17 <pokrovskii.anton@mail.ru>
2025-04-03 22:03:45 +0300

    mini refractoring by projection. Filed firstName , lastNme ignore , but fullName is ser to json

--- src/main/java/com/example/springdataprojections/DTO/EmployeeProjection.java
+++ src/main/java/com/example/springdataprojections/DTO/EmployeeProjection.java
@@ -1,8 +1,13 @@
 package com.example.springdataprojections.DTO;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public interface EmployeeProjection {
+
+    @JsonIgnore
     String getFirstName();
+    @JsonIgnore
     String getLastName();
     String getPosition();
     String getDepartmentName();
